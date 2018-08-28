7747d1816b1a37a51a57a1ef9912d101d9a09d47
VGGeorgiev <vggeorgiev@mail.bg>
2018-08-28 13:45:21 +0300

    Fixed dependency loader

---
+++ LearningSystem.Services/UserService.cs
@@ -0,0 +1,150 @@
+namespace LearningSystem.Infrastructure.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using LearningSystem.Common;
+    using LearningSystem.Core.Entities;
+    using LearningSystem.Core.Repositories;
+    using LearningSystem.Core.Services;
+
+    public class UserService : IUserService
+    {
+        private IRepository<User> usersRepository;
+
+        public UserService(IRepository<User> usersRepository)
+        {
+            this.usersRepository = usersRepository;
+        }
+
+        public User Authenticate(string username, string password)
+        {
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                return null;
+            }
+            
+            var user = this.usersRepository.GetAll().SingleOrDefault(x => x.Username == username);
+
+            // check if username exists
+            if (user == null)
+            {
+                return null;
+            }
+
+            // check if password is correct
+            if (!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
+            {
+                return null;
+            }
+
+            // authentication successful
+            return user;
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            return this.usersRepository.GetAll();
+        }
+
+        public User GetById(int id)
+        {
+            return this.usersRepository.Get(id);
+        }
+
+        public User Create(User user, string password)
+        {
+            // validation
+            if (string.IsNullOrWhiteSpace(password))
+                throw new AppException("Password is required");
+
+            if (this.usersRepository.GetAll().Any(x => x.Username == user.Username))
+                throw new AppException("Username \"" + user.Username + "\" is already taken");
+
+            byte[] passwordHash, passwordSalt;
+            CreatePasswordHash(password, out passwordHash, out passwordSalt);
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            this.usersRepository.Insert(user);
+
+            return user;
+        }
+
+        public void Update(User userParam, string password = null)
+        {
+            var user = this.usersRepository.Get(userParam.Id);
+
+            if (user == null)
+                throw new AppException("User not found");
+
+            if (userParam.Username != user.Username)
+            {
+                // username has changed so check if the new username is already taken
+                if (this.usersRepository.GetAll().Any(x => x.Username == userParam.Username))
+                    throw new AppException("Username " + userParam.Username + " is already taken");
+            }
+
+            // update user properties
+            user.FirstName = userParam.FirstName;
+            user.LastName = userParam.LastName;
+            user.Username = userParam.Username;
+
+            // update password if it was entered
+            if (!string.IsNullOrWhiteSpace(password))
+            {
+                byte[] passwordHash, passwordSalt;
+                CreatePasswordHash(password, out passwordHash, out passwordSalt);
+
+                user.PasswordHash = passwordHash;
+                user.PasswordSalt = passwordSalt;
+            }
+
+            this.usersRepository.Update(user);
+        }
+
+        public void Delete(int id)
+        {
+            var user = this.usersRepository.Get(id);
+            if (user != null)
+            {
+                this.usersRepository.Delete(user);
+            }
+        }
+
+        // private helper methods
+
+        private static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            if (password == null) throw new ArgumentNullException("password");
+            if (string.IsNullOrWhiteSpace(password)) throw new ArgumentException("Value cannot be empty or whitespace only string.", "password");
+
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        private static bool VerifyPasswordHash(string password, byte[] storedHash, byte[] storedSalt)
+        {
+            if (password == null) throw new ArgumentNullException("password");
+            if (string.IsNullOrWhiteSpace(password)) throw new ArgumentException("Value cannot be empty or whitespace only string.", "password");
+            if (storedHash.Length != 64) throw new ArgumentException("Invalid length of password hash (64 bytes expected).", "passwordHash");
+            if (storedSalt.Length != 128) throw new ArgumentException("Invalid length of password salt (128 bytes expected).", "passwordHash");
+
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(storedSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                for (int i = 0; i < computedHash.Length; i++)
+                {
+                    if (computedHash[i] != storedHash[i]) return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ LearningSystem.Web/Helpers/DependencyRegister.cs
@@ -0,0 +1,35 @@
+﻿namespace LearningSystem.Web
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+
+    using Microsoft.Extensions.DependencyInjection;
+    using LearningSystem.Infrastructure;
+    using LearningSystem.Infrastructure.Services;
+
+    public static class DependencyRegister
+    {
+        private const string InterfacePrefix = "I";
+        private static readonly List<Assembly> Assemblies = new List<Assembly>
+        {
+            typeof(DataContext).Assembly, // LearningSystem.Infrastructure
+            typeof(UserService).Assembly // LearningSystem.Services
+        };
+
+        public static void AddProjectDependencies(this IServiceCollection services)
+        {
+            foreach (var assembly in Assemblies)
+            {
+                var types = assembly.GetTypes()
+                    .Where(x => x.IsClass && !x.IsAbstract && !x.IsGenericType)
+                    .Where(x => x.GetInterfaces().Any(i => i.Name == $"{InterfacePrefix}{x.Name}"));
+                foreach (var type in types)
+                {
+                    var typeInterface = type.GetInterface($"{InterfacePrefix}{type.Name}");
+                    services.AddTransient(typeInterface, type);
+                }
+            }
+        }
+    }
+}
