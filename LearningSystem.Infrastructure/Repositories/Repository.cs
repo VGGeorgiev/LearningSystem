ab1caf2199144f6b00c759d741a6fc7ab3d1edf3
VGGeorgiev <vggeorgiev@mail.bg>
2018-09-02 23:20:41 +0300

    Added Include/IncludeThen in Repository

--- LearningSystem.Infrastructure/Repositories/Repository.cs
+++ LearningSystem.Infrastructure/Repositories/Repository.cs
@@ -3,29 +3,36 @@
     using LearningSystem.Core.Entities;
     using LearningSystem.Core.Repositories;
     using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Query;
+    using Microsoft.EntityFrameworkCore.Query.Internal;
     using System;
+    using System.Collections;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Linq.Expressions;
+    using System.Reflection;
 
     public class Repository<T> : IRepository<T> where T : BaseEntity
     {
         protected readonly DataContext context;
         protected DbSet<T> entities;
+        protected IQueryable<T> queriableEntities;
         string errorMessage = string.Empty;
 
         public Repository(DataContext context)
         {
             this.context = context;
             entities = context.Set<T>();
+            queriableEntities = context.Set<T>();
         }
         public IEnumerable<T> GetAll()
         {
-            return entities.AsEnumerable();
+            return queriableEntities.AsEnumerable();
         }
 
         public T Get(int id)
         {
-            return entities.SingleOrDefault(s => s.Id == id);
+            return queriableEntities.SingleOrDefault(s => s.Id == id);
         }
 
         public void Insert(T entity)
@@ -64,6 +71,28 @@
         public void SaveChanges()
         {
             context.SaveChanges();
+        }
+        
+        public IRepository<T> Include<TProperty>(Expression<Func<T, TProperty>> navigationPropertyPath)
+        {
+            queriableEntities = queriableEntities.Include(navigationPropertyPath);
+            return this;
+        }
+        
+        public IRepository<T> ThenInclude<TPreviousProperty, TProperty>(Expression<Func<TPreviousProperty, TProperty>> navigationPropertyPath)
+        {
+            var includableQueryableEntities = queriableEntities as IIncludableQueryable<T, TPreviousProperty>;
+            if (includableQueryableEntities != null)
+            {
+                queriableEntities = includableQueryableEntities.ThenInclude(navigationPropertyPath);
+            }
+            else
+            {
+                var includableQueryableListEntities = queriableEntities as IIncludableQueryable<T, List<TPreviousProperty>>;
+                queriableEntities = includableQueryableListEntities.ThenInclude(navigationPropertyPath);
+            }
+
+            return this;
         }
     }
 }
