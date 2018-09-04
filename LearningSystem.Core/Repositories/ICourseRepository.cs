4fc6f8507a5c677247bd28b4398a2bc316328e49
VGGeorgiev <vggeorgiev@mail.bg>
2018-09-04 21:26:15 +0300

    Updated repositories

--- LearningSystem.Core/Repositories/ICourseRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LearningSystem.Core.Repositories
-{
-    using LearningSystem.Core.Entities;
-
-    public interface ICourseRepository
-    {
-        Course GetDetailedCourse(int id); 
-    }
-}
--- LearningSystem.Core/Repositories/IRepository.cs
+++ LearningSystem.Core/Repositories/IRepository.cs
@@ -1,6 +1,8 @@
 ﻿namespace LearningSystem.Core.Repositories
 {
+    using System;
     using System.Collections.Generic;
+    using System.Linq.Expressions;
     using LearningSystem.Core.Entities;
 
     public interface IRepository<T> where T : BaseEntity
@@ -16,5 +18,9 @@
         void Delete(T entity);
         
         void SaveChanges();
+
+        IRepository<T> Include<TProperty>(Expression<Func<T, TProperty>> navigationPropertyPath);
+
+        IRepository<T> ThenInclude<TPreviousProperty, TProperty>(Expression<Func<TPreviousProperty, TProperty>> navigationPropertyPath);
     }
 }
